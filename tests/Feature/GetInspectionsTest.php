0c4fb0fca21565ce5ff3a78cda8c16caaafc61b2
Dominic <haakym@gmail.com>
2022-12-04 13:25:20 +0000

    add data to get all inspections feature test

--- tests/Feature/GetInspectionsTest.php
+++ tests/Feature/GetInspectionsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class GetInspectionsTest extends TestCase
@@ -15,8 +16,19 @@ class GetInspectionsTest extends TestCase
      */
     public function testGetAllInspections(): void
     {
+        $inspections = Inspection::factory()->count(3)->create();
+
         $response = $this->get('/api/inspections');
 
         $response->assertStatus(200);
+        
+        $inspections->each(function ($item, $key) use ($response) {
+            $response->assertJson([
+                'id' => $item->id,
+                'date_time' => $item->date_time,
+                'turbine_id' => $item->turbine_id,
+                'pilot_id' => $item->pilot_id,
+            ]);
+        });
     }
 }
